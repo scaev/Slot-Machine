3c3a0f6324f72fb0c29d0fd41ed4063001e7b5d1
scaev <kenkaraa@gmail.com>
2023-01-02 12:37:20 -0500

    Update 1.0.2 - Add function

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ let reels = ["", "", ""], // array to hold the symbols for each reel
   winnings = 0, // current winnings for the player
   jackpot = 5000, // current jackpot amount
   spinResult; // result of the current spin
+
+function init() {
+  for (let i = 0; i < reels.length; i++) {
+    reels[i] = "blank";
+  }
+  for (let i = 0; i < symbols.length; i++) {
+    symbols[i] = symbols[i];
+  }
+}
+
+document.getElementById("reel1").innerHTML = symbols[0];
+document.getElementById("reel2").innerHTML = symbols[2];
+document.getElementById("reel3").innerHTML = symbols[1];
