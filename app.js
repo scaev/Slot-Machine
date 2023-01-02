41f7a46ff147f60c8325cd7d5f3eedd9c5dc2eef
scaev <kenkaraa@gmail.com>
2023-01-02 12:01:03 -0500

    Update 1.0.1 - Edit HTML,CSS and define variables

---
+++ app.js
@@ -0,0 +1,7 @@
+let reels = ["", "", ""], // array to hold the symbols for each reel
+  symbols = ["🍌", "🍕", "🍾"], // array to hold the possible symbols
+  playerMoney = 1000, // starting money for the player
+  playerBet = 0, // current bet for the player
+  winnings = 0, // current winnings for the player
+  jackpot = 5000, // current jackpot amount
+  spinResult; // result of the current spin
