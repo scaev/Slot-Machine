ca9532e57e9eabcd5164cb05b98b5cf2cf6fee5d
scaev <kenkaraa@gmail.com>
2023-01-07 14:45:06 -0500

    Update 2 - Orginizing files and code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let reels = ["", "", ""], // array to hold the symbols for each reel
-  symbols = ["🍌", "🍕", "🍾", "👻"], // array to hold the possible symbol
+  symbols = ["🍌", "🍕", "🍾", "👻"], // array to hold the possible symbols
   playerMoney = 1000, // starting money for the player
   playerBet = 0, // current bet for the player
   winnings = 0; // current winnings for the player
@@ -7,7 +7,7 @@ negative = new Audio("sounds/negative.mp3");
 coin = new Audio("sounds/coin.mp3");
 
 // Spin the reels and determine the result
-function spin() {
+function randomValue() {
   for (let i = 0; i < reels.length; i++) {
     let randomIndex = Math.floor(Math.random() * symbols.length);
     reels[i] = symbols[randomIndex];
@@ -24,22 +24,16 @@ function determineWinnings() {
   if (reels[0] == reels[1] && reels[1] == reels[2]) {
     winnings = playerBet * 10;
     playerMoney = playerMoney + winnings;
-    // document.getElementById("result").innerHTML = "YOU WON";
-    // document.getElementById("result").style.background = "#75f56e";
     $(result).html("YOU WON");
     $(result).css({ background: "#75f56e" });
     coin.play();
   } else {
     winnings = 0;
     playerMoney = playerMoney - playerBet;
-    // document.getElementById("result").innerHTML = "YOU LOST";
-    // document.getElementById("result").style.background = "#70231F";
     $("#result").html("YOU LOST");
     $("#result").css({ background: "#70231F" });
     negative.play();
   }
-  // document.getElementById("money").innerHTML = `Money: $${playerMoney}`;
-  // document.getElementById("winnings").innerHTML = `Winnings: $${winnings}`;
   $("#money").html(`Money: $${playerMoney}`);
   $("#winnings").html(`Winnings: $${winnings}`);
 }
@@ -47,14 +41,12 @@ function determineWinnings() {
 // Add event listener for the mute button
 document.getElementById("muteSound").addEventListener("click", function () {
   if ((document.getElementById("muteSound").innerHTML = "🔊")) {
-    // document.getElementById("muteSound").innerHTML = "🔇";
     $(muteSound).html("🔇");
     negative.muted = true;
     coin.muted = true;
   }
   document.getElementById("muteSound").addEventListener("click", function () {
     if ((document.getElementById("muteSound").innerHTML = "🔇")) {
-      // document.getElementById("muteSound").innerHTML = "🔊";
       $(muteSound).html("🔊");
       negative.muted = false;
       coin.muted = false;
@@ -66,13 +58,10 @@ document.getElementById("muteSound").addEventListener("click", function () {
 $(spinButton).click(function () {
   let betInput = document.getElementById("betInput");
   let playerBet = betInput.valueAsNumber;
+
   // Check if the player has run out of money
   if (playerMoney <= 0) {
     alert("You have run out of money!");
-    return;
-  }
-  if (isNaN(playerBet)) {
-    alert("Please enter your bet.");
     return;
   }
   if (playerBet > playerMoney) {
@@ -81,8 +70,11 @@ $(spinButton).click(function () {
     );
     return;
   }
-  spin();
-
+  if (isNaN(playerBet)) {
+    alert("Please enter your bet.");
+    return;
+  }
+  randomValue();
   determineWinnings();
   // startingMoney.style.display = "none";
   $(startingMoney).hide();
